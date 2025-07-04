403d14a9ade8c81be55f2ada53a464807d2a6620
Kevin <kevinkwokb@gmail.com>
2025-07-04 13:50:31 -0700

    Added Tickets.cpp for Dodgers ticket calculator program

---
+++ Tickets.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double FRONT_ROW_PRICE = 110;
+    double REGULAR_PRICE = 60;
+    double TAX_RATE = .0725;//7.25 tax
+
+    int frontRowTickets, regularTickets;
+    
+}
